6968ca3ad8be82353e7ffb34c95cc6e6a98c7656
alan <asebastian2@gmail.com>
2012-05-16 10:51:41 -0700

    Better error message for site_url not activated (properties api).

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -88,7 +88,7 @@ class Api::PropertiesController < ApplicationController
       if @user.authentication_token == "NULL"
         respond_error("Your token is invalid. Please make sure your subscription is still active.")
       elsif @user.site_url != request.referer
-        respond_error("This site #{request.referer} is not activated. Please deactivate #{@user.site_url} first.") 
+        respond_error("This site (#{request.referer}) is not activated. Please activate this site, then try again.")
       elsif @user.site_url == "NULL"
         respond_error("You have not activated a site on this token yet.") 
       end
