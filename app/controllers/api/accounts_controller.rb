c915cd0f716ad70312cbb8e1792be5bd22c15f36
alan <asebastian2@gmail.com>
2012-05-16 11:56:15 -0700

    This better work (part 2).

--- app/controllers/api/accounts_controller.rb
+++ app/controllers/api/accounts_controller.rb
@@ -19,7 +19,7 @@ class Api::AccountsController < ApplicationController
       if User.find_by_authentication_token(params[:Token])
         @user = User.find_by_authentication_token(params[:Token])
 
-        @user.site_url = request.env['REQUEST_URI']
+        @user.site_url = request.env['HTTP_HOST'] + '/' + request.env['REQUEST_URI']
         @user.site_ip_address = request.remote_ip
 
         if @user.save
