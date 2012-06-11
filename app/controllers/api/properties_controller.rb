5e9a4d0d409de538b706eff362ed8261792a79e3
alan <asebastian2@gmail.com>
2012-06-11 16:17:27 -0700

    Add batsd submodule back in

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -1,7 +1,7 @@
 class Api::PropertiesController < ApplicationController
   # TODO: figure out how to dry this up.
   include Api::Shared::ErrorsHelper
-  Batsd = Api::Shared::BatsdHelper
+  include Api::Shared::BatsdHelper
 
   require 'socket'
 
--- app/helpers/api/shared/batsd_helper.rb
+++ app/helpers/api/shared/batsd_helper.rb
@@ -2,39 +2,42 @@ module Api::Shared::BatsdHelper
   # TODO: move logger calls into respond_error and respond_success
   # This module attempts to do as much logging as possible with as few methods
   # as possible. The idea is to keep as calls to the logger as possible. 
+
   # Dynamically increments a batsd counter based on HTTP_REFERER 
-  # :success => boolean (was the request successful?)
-  # :error_type => :auth, :referer, :params
-  def self.increment(options={})
-    if @user == nil
-      if !request.env["HTTP_REFERER"]
-        @ctr_token = "UNKNOWN"
+  # :success => boolean (was the @request successful?)
+  # :error_type => :auth, :referer, :@params
+  module Batsd
+    def self.increment(options={})
+      if @user == nil
+        if !@request.env["HTTP_REFERER"]
+          @ctr_token = "UNKNOWN"
+        else
+          http_ref = @request.env["HTTP_REFERER"].gsub(/http[s]?:\/\//, "")
+          @ctr_token = "UNKOWN-at-" + http_ref
+        end
       else
-        http_ref = request.env["HTTP_REFERER"].gsub(/http[s]?:\/\//, "")
-        @ctr_token = "UNKOWN-at-" + http_ref
+        @ctr_token = @user.authentication_token
       end
-    else
-      @ctr_token = @user.authentication_token
-    end
-    ctr_req = params[:controller] + '.' + params[:action]
-    counter = @ctr_token + "." + ctr_req
+      ctr_req = @params[:controller] + '.' + @params[:action]
+      counter = @ctr_token + "." + ctr_req
 
-    if options[:success] == false
-      counter = counter + ".fail"
-    elsif options[:success] == true
-      counter = counter + ".success"
-    end
+      if options[:success] == false
+        counter = counter + ".fail"
+      elsif options[:success] == true
+        counter = counter + ".success"
+      end
 
-    if options[:error_type] == :auth
-      counter = counter + ".auth"
-    elsif options[:error_type] == :referer
-      counter = counter + ".referer"
-    elsif options[:error_type] == :params
-      counter = counter + ".params"
-    end
+      if options[:error_type] == :auth
+        counter = counter + ".auth"
+      elsif options[:error_type] == :referer
+        counter = counter + ".referer"
+      elsif options[:error_type] == :@params
+        counter = counter + ".params"
+      end
 
-    # log stat
-    $statsd.increment(counter)
+      # log stat
+      $statsd.increment(counter)
+    end
   end
 end
 
