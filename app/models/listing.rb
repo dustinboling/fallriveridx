7e818a04ff78beac6598b372815eda247e74c082
alan <asebastian2@gmail.com>
2012-06-11 11:16:07 -0700

    Update destroy:listings task with counters.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,12 @@
 class Listing < ActiveRecord::Base
   # set primary key to listing ID, necessary 
   # for eager loading of prop_media
-  self.primary_key = "ListingID"
+  # self.primary_key = "ListingID"
 
   belongs_to :agent
   belongs_to :broker
 
-  has_many :property_media, :primary_key => :ListingID, :foreign_key => :ListingID
+  # has_many :property_media, :primary_key => :ListingID, :foreign_key => :ListingID
 
   def self.less_than(property, value)
     where("\"#{property}\" < ?", "#{value}")
--- lib/tasks/destroy.rake
+++ lib/tasks/destroy.rake
@@ -2,6 +2,13 @@ namespace :destroy do
 
   desc "destroy listings"
   task :listings => :environment do 
-    Listing.find_each(&:destroy)
+    count = Listing.count
+    puts "Total number of listings: #{count}"
+    destroy_count = 0
+    Listing.find_each do |listing|
+      Listing.delete
+      destroy_count = destroy_count + 1
+    end
+    puts "Total listings destroyed: #{destroy_count}"
   end
 end
