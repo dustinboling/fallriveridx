f66323d3424c0c1c091f85a9fae1b2e3fcec8420
alan <asebastian2@gmail.com>
2012-06-22 11:07:31 -0700

    More new migrations.

---
+++ db/migrate/20120622171012_drop_all_indexes_for_real_this_time.rb
@@ -0,0 +1,13 @@
+class DropAllIndexesForRealThisTime < ActiveRecord::Migration
+  def up
+    remove_index :listings, :ListingID
+    remove_index :listings, :ListingKey
+    remove_index :listings, :FullStreetAddress
+    remove_index :listings, :City
+    remove_index :listings, :Latitude
+    remove_index :listings, :Latitude
+  end
+
+  def down
+  end
+end
--- db/migrate/20120622171630_add_indexes_back_on_listings.rb
+++ db/migrate/20120622171630_add_indexes_back_on_listings.rb
@@ -2,6 +2,12 @@ class AddIndexesBackOnListings < ActiveRecord::Migration
   def up
     add_index :listings, :BathsTotal
     add_index :listings, :BedroomsTotal
+    add_index :listings, :ListingID
+    add_index :listings, :ListingKey
+    add_index :listings, :FullStreetAddress
+    add_index :listings, :City
+    add_index :listings, :Latitude
+    add_index :listings, :Longitude
     execute <<-SQL
       create index idx_query on listings ("ListPrice", "BedroomsTotal", "BathsTotal", "BuildingSize", "LotSizeSQFT", "Latitude", "Longitude");
       create index idx_shortcode_search on listings ("City", "ListPrice", "BedroomsTotal", "BathsTotal", "BuildingSize", "ListingStatus");
