e8e46fa3b1548c612c203308fab9b775fb4c9f50
alan <asebastian2@gmail.com>
2012-08-06 11:22:15 -0700

    Add some controller specs

--- spec/controllers/api/autocomplete_controller_spec.rb
+++ spec/controllers/api/autocomplete_controller_spec.rb
@@ -2,4 +2,32 @@ require 'spec_helper'
 
 describe Api::AutocompleteController do
 
+  describe "GET cities" do
+    it "returns http success" do
+      get :cities
+      response.should be_success
+    end
+  end
+
+  describe "GET tracts" do
+    it "returns http success" do
+      get :tracts
+      response.should be_success
+    end
+  end
+
+  describe "GET communities" do
+    it "returns http success" do
+      get :communities
+      response.should be_success
+    end
+  end
+
+  describe "GET combined" do
+    it "returns http success" do
+      get :combined
+      response.should be_success
+    end
+  end
+
 end
--- spec/controllers/api/geocode_controller_spec.rb
+++ spec/controllers/api/geocode_controller_spec.rb
@@ -2,9 +2,16 @@ require 'spec_helper'
 
 describe Api::GeocodeController do
 
-  describe "GET 'index'" do
+  describe "GET index" do
     it "returns http success" do
-      get 'index'
+      get :index
+      response.should be_success
+    end
+  end
+
+  describe "GET geocode" do
+    it "returns http success" do
+      get :geocode
       response.should be_success
     end
   end
