682749bafddbe0b4137b606f873174711c4f6038
alan <asebastian2@gmail.com>
2012-08-08 15:18:12 -0700

    Drop limit down to 300

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -68,7 +68,7 @@ class Api::PropertiesController < ApplicationController
       elsif @order_by
         query = query + @order_by + ";"
       elsif !@order_by && !@query_limit
-        query = query + "ORDER BY \"ListingDate\" DESC LIMIT 500;"
+        query = query + "ORDER BY \"ListingDate\" DESC LIMIT 300;"
       else
         batsd_log_error(:type => :params)
         respond_error("No parameters supplied.")
