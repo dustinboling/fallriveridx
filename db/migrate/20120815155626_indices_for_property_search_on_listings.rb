622edc8fff8ab4a65b86b26840312e46f7e0a94e
alan <asebastian2@gmail.com>
2012-08-15 09:15:43 -0700

    Remove long  listing inices from migration

--- db/migrate/20120815155626_indices_for_property_search_on_listings.rb
+++ db/migrate/20120815155626_indices_for_property_search_on_listings.rb
@@ -1,8 +1,5 @@
 class IndicesForPropertySearchOnListings < ActiveRecord::Migration
   def change
     add_index :listings, :ListingDate
-    add_index :listings, [:ListingStatus, :City, :ListPrice, :ListingDate]
-    add_index :listings, [:ListingStatus, :City, :ListPrice, :BedroomsTotal, :ListingDate]
-    add_index :listings, [:ListingStatus, :City, :ListPrice, :BathsTotal, :ListingDate]
   end
 end
