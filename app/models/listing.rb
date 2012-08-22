3716d4a5fd3ebea65f00a987197107ac78f3de19
alan <asebastian2@gmail.com>
2012-08-22 11:03:22 -0700

    Only get jpegs for index json

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -10,7 +10,7 @@ class Listing < ActiveRecord::Base
 
   def self.find_by_sql_with_associations(query)
     listings = find_by_sql(query)
-    ActiveRecord::Associations::Preloader.new(listings, [:property_media, {:PropMimeType => 'IMAGE_GIF'}]).run
+    ActiveRecord::Associations::Preloader.new(listings, [:property_media, {:PropMimeType => 'IMAGE_JPEG'}]).run
     return listings
   end
 
