c0af1a782efde55b91e45609552572909ac62ab7
alan <asebastian2@gmail.com>
2012-08-29 15:18:00 -0700

    Skeletons for new api specs

--- spec/controllers/api/properties_controller_spec.rb
+++ spec/controllers/api/properties_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Api::PropertiesController do
+  ###
+  # check for 200 ok
+  ###
   describe "GET index" do
     it "should be successful" do
       get 'index'
@@ -8,6 +11,48 @@ describe Api::PropertiesController do
     end
 
     it "renders some json for an authenticated user" do
+    end
+  end
+
+  describe "GET show" do
+    it "should be successful" do
+      get 'show'
+      response.should be_success
+    end
+  end
+
+  describe "GET invalid_parameters" do
+    it "should be successful" do
+      get 'invalid_parameters'
+      response.should be_success
+    end
+  end
+
+  ###
+  # check action functionality
+  ###
+  describe "validate_params" do
+    it "should accept good parameters" do
+    end
+
+    it "should discard unneccessary params" do
+    end
+
+    it "should raise error on bad parameters" do
+    end
+  end
+
+  describe "authenticate_referer" do
+    it "should make a user submit a token" do
+    end
+
+    it "should reject a null token" do
+    end
+
+    it "should reject a user who has not activated the referring site" do
+    end
+
+    it "should reject a user whose referring site does not equal site_url in database" do
     end
   end
 end
--- vendor/plugins/distribution-sorter/lib/distribution-sorter.rb
+++ vendor/plugins/distribution-sorter/lib/distribution-sorter.rb
@@ -1,14 +1,20 @@
 require 'wukong'
+
+# Provides distibution of values for a specific column for all records of 
+# an Active Record class.
 module DistributionSorter
   class Mapper < Wukong::Streamer::Base
     # set record_klass to the AR class you'd like to batch process
     cattr_accessor :record_klass
     # Size of each batch to pull from db
     cattr_accessor :batch_size
+    # Accepts an array of record columns which will be flattened into a string
+    cattr_accessor :record_columns
 
     def initialize
       # TODO: remove klass from here and just pass it in the new hash
       self.record_klass = PropertyMedium
+      self.record_columns = ['PropMediaPixelLength', 'PropMediaPixelWidth']
       self.batch_size = 100
     end
 
@@ -17,7 +23,17 @@ module DistributionSorter
       before_stream
       record_klass.find_in_batches(:batch_size => self.batch_size) do |record_batch|
         record_batch.each do |record|
-          process(record.id, record) do |output_record|
+          columns = self.record_columns
+          if columns.kind_of?(Array) && columns.count > 1
+            @data = []
+            columns.each do |c|
+              @data << record.c 
+            end
+            @data = @data.join(",")
+          else
+            @data = record.columns
+          end
+          process(record.id, @data) do |output_record|
             emit output_record
           end
         end
@@ -27,10 +43,9 @@ module DistributionSorter
   end
 
   # TODO: which reduce pattern to use?
-  class Reducer < Wukong::Streamer
+  class Reducer # < Wukong::Streamer::
     def finalize
       yield [output_record]
     end
   end
-
 end
